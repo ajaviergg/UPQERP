b30e27cf8c9e3463c49d6f5c857a686aebebcca6
Adrihanx <adrihanx@gmail.com>
2018-04-19 01:58:07 -0500

    SQL Funcionando
    La conexion con SQL es un exito, ya inserta registros.

--- app/Http/Controllers/FinanzasController.php
+++ app/Http/Controllers/FinanzasController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
@@ -16,17 +17,27 @@ class FinanzasController extends Controller
     	return view ('Finanzas.BancosCajas.crearfinanzas');
     }
     public function agregarCuenta(Request $request){
-    	return var_dump($request);
+    	$tipo = $request->input('tipo');
+    	$sucursal = $request->input('sucursal');
+    	$banco = $request->input('banco');
+    	$descripcion = $request->input('descripcion');
+    	$titulo = $request->input('titulo');
+    	$cuenta = $request->input('cuenta');
+    	$clabe = $request->input('clabe');
+    	$balance_inicial = $request->input('balance_inicial');
+    	$fechabalance_inicial = $request->input('fechabalance_inicial');
+    	$moneda = $request->input('moneda');
+    	$descripcion = $request->input('descripcion');
     	DB::table('fin_cuenta')->insert(
-    		['id_tipo'=>'',
-    		'id_banco'=>'',
-    		'balance_inicial'=>'',
-    		'id_moneda'=>'',
-    		'id_sucursal'=>'',
-    		'titulo'=>'',
-    		'clave'=>'',
-    		'fecha_balance_inicial'=>'',
-    		'descripcion'=>'']
+    		['id_tipo'=>$tipo,
+    		'id_banco'=>$banco,
+    		'balance_inicial'=>$balance_inicial,
+    		'id_moneda'=>$moneda,
+    		'id_sucursal'=>$sucursal,
+    		'titulo'=>$titulo,
+    		'clave'=>$clabe,
+    		'fecha_balance_inical'=>$fechabalance_inicial,
+    		'descripcion'=>$descripcion]
     	);
     }
 }
